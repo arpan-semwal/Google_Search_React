4ff8032600b3ea996e66c485238dd7182613ecfd
arpan-semwal <arpansemwal8@gmail.com>
2023-09-21 22:05:26 +0530

    updateSearchInput

--- google-search-app/src/components/SearchInput.jsx
+++ google-search-app/src/components/SearchInput.jsx
@@ -4,63 +4,60 @@ import { IoMdClose } from "react-icons/io";
 import { useNavigate, useParams } from "react-router-dom";
 import MicIcon from "../assets/mic.svg";
 import ImageIcon from "../assets/image.svg";
-import {useSpeechRecognition} from "react-speech-recognition";
 
 const SearchInput = () => {
-    const {query} = useParams();
+  const { query } = useParams();
+  const [searchQuery, setSearchQuery] = useState(query || "");
+  const navigate = useNavigate();
 
-    const [searchQuery , setSearchQuery] = useState(query || "");
-
-    const navigate = useNavigate();
-
-   const{
-    transcript,
-    resetTranscript,
-    startListening,
-    stopListening,
-    listening
-   } = useSpeechRecognition();
-
-
-    const searchQueryHandler = () => {
-        if(event.key === "Enter" && searchQuery.length > 0){
-            navigate(`${searchQuery}/${1}`);
-        }
+  const searchQueryHandler = () => {
+    if (searchQuery.length > 0) {
+      navigate(`/${searchQuery}/${1}`);
     }
+  };
 
-    const voiceSearchHandler = () => {
-        if (listening) {
-            // If currently listening, stop listening
-            stopListening();
-        } else {
-            // If not listening, start listening
-            startListening();
-        }
-    };
+  const clearSearchQuery = () => {
+    setSearchQuery("");
+  };
 
+  return (
+    <div
+      id="searchbox"
+      className="h-[48px] w-full md:w-[584px] flex items-center gap-3 px-4 border border-[#dfe1e5] rounded-3xl hover:bg-white hover:shadow-c hover:border-0 focus-within:shadow-c focus-within:border-0"
+    >
+      <AiOutlineSearch size={18} color="#9aa0a6" />
+      <input
+        type="text"
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onKeyUp={(e) => {
+          if (e.key === "Enter") {
+            searchQueryHandler();
+          }
+        }}
+        value={searchQuery}
+        autoFocus
+        className="grow outline-0 text-black"
+      />
 
-
-    return <div id="searchbox" className="h-[48px] w-full md:w-[584px] flex items-center gap-3 px-4 border border-[#dfe1e5] rounded-3xl hover:bg-white hover:shadow-c hover:border-0 focus-within:shadow-c focus-within:border-0">
-        <AiOutlineSearch size={18} color="#9aa0a6"/>
-        <input type="text" onChange={(e) =>setSearchQuery(e.target.value)} onKeyUp={searchQueryHandler} value={searchQuery} autoFocus className="grow outline-0 text-black"
-    />
-
-    <div className="flex items-center gap-3">
+      <div className="flex items-center gap-3">
         {searchQuery && (
-            <IoMdClose size={24} color="#70757a" className="cursor-pointer"
-            onClick={() => setSearchQuery("")}
-            
-            />
+          <IoMdClose
+            size={24}
+            color="#70757a"
+            className="cursor-pointer"
+            onClick={clearSearchQuery}
+          />
         )}
         <img
-                    className="h-6 w-6 cursor-pointer"
-                    src={listening ? ImageIcon : MicIcon} // Toggle between MicIcon and ImageIcon based on listening state
-                    alt=""
-                    onClick={voiceSearchHandler} // Call voiceSearchHandler on click
-                />
+          className="h-6 w-6 cursor-pointer"
+          src={MicIcon}
+          alt=""
+          onClick={searchQueryHandler}
+        />
         <img className="h-6 w-6 cursor-pointer" src={ImageIcon} alt="" />
+      </div>
     </div>
-</div>;
+  );
 };
 
 export default SearchInput;
